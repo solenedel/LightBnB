e88e16b69cf29a3943a455258083e35295ba203c
Solene D <solene98@student.ubc.ca>
2021-08-19 18:06:28 +0000

    modify getUserWithEmail to take in user input

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,16 +18,31 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+
+  return pool
+  .query(`SELECT * FROM users
+          WHERE email = $1`, [email])
+  .then(result => {
+    // if there is a user associated with the email 
+    if (result.rows[0]) {
+      return result.rows[0];
     } else {
-      user = null;
+      // if there is no user for that email
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch(error => console.log(error.message));
+
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
